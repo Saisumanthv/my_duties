7f6453bf4a1695c8607aa5527fc4c6bd9f2044a0
Sai Sumanth Vadla <104812692+Saisumanthv@users.noreply.github.com>
2025-09-14 00:49:36 +0530

    v1.0

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+
+st.title("Everything to do as an :blue[ASE]")
+st.subheader("`Sai Sumanth Vadla` :sunglasses:")
+st.divider()
+
+st.markdown(":yellow[1.] `My Daily Tracking Sheet:`")
+st.link_button("My Daily Tracking Sheet", "https://docs.google.com/spreadsheets/d/1zWlaxQ4tgGOT20otA6isgoP3q1SirNaCJQNAoNZ3_vg/edit?gid=0#gid=0")
+st.divider()
+
+st.markdown("⬇️ You can find all the sheets and credentials below ⬇️")
+st.link_button("All Sheets & Credentials", "https://docs.google.com/spreadsheets/d/1R22oyaam7x2iplHreRTBp4L0UuqnMFLKIHxBD9ssACs/edit?gid=0#gid=0")
+st.divider()
+
+st.markdown(":yellow[2.] Whenever there is a company interaction, take a mock interview for the students and fill their feedback in this form. You can find each company subsheet in this google sheet ⬇️")
+st.link_button("Students Mock Interview Feedback & Roadmap Completion Update", "https://docs.google.com/spreadsheets/d/1b7d5v2dNjipHbz7DJETdgwKVMn7tUk-0Ox3NjcDW4Tw/edit?gid=939972995#gid=939972995")
+st.markdown(":red[Note:] You can find link to the company roadmap in this subsheet itself. Update that roadmap according to the students topics completion. If you can't find the link to roadmap in this subsheet, you can find every company roadmap from the below link ⬇️, add it there by taking the link from the below google drive. And don't forget to add Interaction/Interview feedback subsheet in there for company POC to fill.")
+st.link_button("All Companies Roadmaps", "https://drive.google.com/drive/folders/12wpcGFvjEVGgPfFa0pl0lfM6jvJTd-l5")
+st.divider()
+
+st.markdown(":yellow[3.] September Month Interactions & Interviews Schedule ⬇️")
+st.link_button("September Month Interactions & Interviews", "https://docs.google.com/spreadsheets/d/1cqop7jqHsBjCyMIC59EU_m-EjxP78WYbiekZMytJeUQ/edit?gid=159424648#gid=159424648")
+st.divider()
+
+st.markdown(":yellow[4.] List of all students in the college with their details ⬇️")
+st.link_button("All Students Details", "https://docs.google.com/spreadsheets/d/1cqop7jqHsBjCyMIC59EU_m-EjxP78WYbiekZMytJeUQ/edit?gid=610743122#gid=610743122")
+st.divider()
+
+st.markdown(":yellow[5.] List of all the current GenAI students ⬇️")
+st.link_button("Current GenAI students", "https://docs.google.com/spreadsheets/d/1WdcbOoOiGYcN720VXlXMAuXdScX2xpImkjd-jtAOuko/edit?gid=591602549#gid=591602549")
+st.divider()
+
+st.markdown(":yellow[6.] ASE's assigned to different stacks ⬇️")
+st.link_button("Stack specific ASE's", "https://docs.google.com/spreadsheets/d/1OS4kVJTfpUB40frVLaNiM6p_UWyCe1rmFdaGuT4hqYw/edit?gid=514316637#gid=514316637")
+st.divider()
+
+st.markdown(":yellow[7.] We need to keep track of student's work progress even after getting selected for an internship ⬇️")
+st.link_button("Post internship student working progress updater", "https://docs.google.com/spreadsheets/d/1OS4kVJTfpUB40frVLaNiM6p_UWyCe1rmFdaGuT4hqYw/edit?gid=1851147861#gid=1851147861")
+st.divider()
+
+st.markdown(":yellow[8.] All GenAI students resumes ⬇️")
+st.link_button("GenAI students resumes", "https://docs.google.com/spreadsheets/d/19CdhWpnSZ1Jb7gM_pU3kn7soSSHrZxHB4Tp3YfD661s/edit?gid=0#gid=0")
+st.divider()
+
+st.markdown(":yellow[9.] Some students are being remapped to other companies or being ready to be remapped. You can check their details here ⬇️")
+st.link_button("Remapping details", "https://docs.google.com/spreadsheets/d/1TmBo6FcBxPWB_gtJj4jX6nEujDvBa9FdDK_qkZAirQE/edit?gid=0#gid=0")
+st.divider()
+
+st.markdown(":yellow[10.] GenAI students Attendence ⬇️")
+st.link_button("GenAI students Attendence", "https://docs.google.com/spreadsheets/d/16pjIpAKzXaI9J1ZWf79axdLu5ofpwvSi-eur83sAKc0/edit?gid=2069598558#gid=2069598558")
+st.divider()
+
+st.markdown(":yellow[11.] Check GenAI students progress regularly ⬇️")
+st.link_button("GenAI students progress tracker", "https://docs.google.com/spreadsheets/d/16pjIpAKzXaI9J1ZWf79axdLu5ofpwvSi-eur83sAKc0/edit?gid=1462103390#gid=1462103390")
+st.divider()
+
+st.markdown(":yellow[12.] GenAI previously asked questions ⬇️")
+st.link_button("Previously asked GenAI questions", "https://docs.google.com/spreadsheets/d/1R22oyaam7x2iplHreRTBp4L0UuqnMFLKIHxBD9ssACs/edit?gid=1803247298#gid=1803247298")
+st.divider()
+
+st.markdown(":yellow[14.] Assign this GenAI projects to students ⬇️")
+st.link_button("Common GenAI Projects that every student has to do", "https://drive.google.com/drive/folders/13u9Q1U8IWc_2GRbnZstWasR6HCLkXF6-?usp=drive_link")
+st.link_button("GenAI Projects by Sirisha Ma'am", "https://docs.google.com/document/d/17PFs2VkRlO2ElKGkA0LxlhKT1dcc81xc/edit?usp=sharing&ouid=108014161482233392531&rtpof=true&sd=true")
+st.link_button("Some more example GenAI Projects", "https://docs.google.com/document/d/1gW-1C9JkMnw4Ms93q8zPeBi8XlqeYsXeyMdiovHAuQM/edit?usp=sharing")
+st.divider()
+
+st.markdown(":yellow[15.] All GenAI PPTs ⬇️")
+st.link_button("GenAI PPTs by Sirisha Ma'am", "https://drive.google.com/drive/folders/1buV_SMDd98_FcxgwPeZlHreJgbaLCxzQ?usp=sharing")
+st.link_button("GenAI PPTs by Chandrashekar sir", "https://drive.google.com/drive/folders/1yCeRBMAwcGXbj3blAqQDc8G5sqjww84z?usp=sharing")
+st.divider()
+
+st.markdown(":yellow[16.] Top GenAI interview questions provided by Sirisha ma'am and Chandrashekar sir ⬇️")
+st.link_button("Top GenAI interview questions", "https://drive.google.com/drive/folders/1DsDjdalYm_oofNuhDZZQ3CsPxmnjWcBQ?usp=sharing")
+st.divider()
